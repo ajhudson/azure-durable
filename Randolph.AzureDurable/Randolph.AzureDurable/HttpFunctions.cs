f88f9c5270bdfd4e5afe90dcff6b9c6f0f03927c
Andy Hudson <andyhudson1979@gmail.com>
2022-09-27 19:47:01 +0100

    Add orchestration function and move to external file

---
+++ Randolph.AzureDurable/Randolph.AzureDurable/HttpFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Randolph.AzureDurable;
+
+public static class HttpFunctions
+{
+    [FunctionName(nameof(ProcessVideoStarter))]
+    public static async Task<HttpResponseMessage> ProcessVideoStarter(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]
+        HttpRequestMessage req,
+        [DurableClient] IDurableOrchestrationClient starter,
+        ILogger log)
+    {
+        // Function input comes from the request content.
+        string instanceId = await starter.StartNewAsync("Starter", null);
+
+        log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+        return starter.CreateCheckStatusResponse(req, instanceId);
+    }
+}
---
+++ Randolph.AzureDurable/Randolph.AzureDurable/Starter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace Randolph.AzureDurable;
+
+public static class Starter
+{
+    [FunctionName("Starter")]
+    public static async Task<List<string>> RunOrchestrator(
+        [OrchestrationTrigger] IDurableOrchestrationContext context)
+    {
+        var outputs = new List<string>();
+
+        // Replace "hello" with the name of your Durable Activity Function.
+        outputs.Add(await context.CallActivityAsync<string>("Starter_Hello", "Tokyo"));
+        outputs.Add(await context.CallActivityAsync<string>("Starter_Hello", "Seattle"));
+        outputs.Add(await context.CallActivityAsync<string>("Starter_Hello", "London"));
+
+        // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+        return outputs;
+        
+    }
+
+    [FunctionName("Starter_Hello")]
+    public static async Task<string> SayHello([ActivityTrigger] string name, ILogger log)
+    {
+        log.LogInformation($"Saying hello to {name}.");
+        return $"Hello {name}!";
+    }
+}
